03cba4869e9e73b1bca729187cca7806ed239d32
rf-developer <reedyfeggins@google.com>
2022-06-15 19:18:05 +0000

    ocr files

--- ocr-tutorial/setup.sh
+++ ocr-tutorial/setup.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/bash
+# checkout https://cloud.google.com/python/docs/setup
+
+export PS1="$ "
 python -m venv env
 source env/bin/activate
 pip install google-cloud-storage
 
+
+xport PROJECT=$(gcloud config get-value project)
+export REGION=us-east1
+
+
+
+#Setup Topics 
+export GCS_NOTIFICATION_TOPIC="gcs-notification-topic"
+gcloud pubsub topics create ${GCS_NOTIFICATION_TOPIC}
+
+export  GCS_NOTIFICATION_SUBSCRIPTION="gcs-notification-subscription"
+gcloud pubsub subscriptions create  ${GCS_NOTIFICATION_SUBSCRIPTION}  --topic=${GCS_NOTIFICATION_TOPIC}
+
+
+export IMAGE_BUCKET=${PROJECT}-images
+gsutil mb -c standard -l ${REGION} gs://${IMAGE_BUCKET}
